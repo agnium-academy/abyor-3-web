4f6202ca12371f34d1b51f121dd2bafebd6072df
trioandianto <trio.andianto@basajans.com>
2016-02-18 00:17:21 +0700

    Menambahkan Project Sistem Informasi Akademik Sekolah

---
+++ Abyor3/MainLayout.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MainLayout.aspx.cs" Inherits="Abyor3.MainLayout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ Abyor3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Abyor3.Startup))]
+namespace Abyor3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
